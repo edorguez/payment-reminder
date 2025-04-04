dcc37cad6c612db91bfe89af6f3968afea2901e5
edorguez <edro998@gmail.com>
2025-04-04 13:22:54 -0400

    Change ID property name to stantards

--- internal/alert/models/alert.go
+++ internal/alert/models/alert.go
@@ -4,9 +4,8 @@ import "time"
 
 type Alert struct {
 	ID              int64         `gorm:"primaryKey;type:bigserial"`
-	IDUser          int64         `gorm:"type:bigserial;not null"`
-	IDAlertTemplate int64         `gorm:"type:bigserial;not null"`
-	AlertTemplate   AlertTemplate `gorm:"foreignKey:AlertTemplateID;references:ID"`
+	UserID          int64         `gorm:"type:bigserial;not null"`
+	AlertTemplateID int64         `gorm:"type:bigserial;not null"`
 	Name            string        `gorm:"type:varchar(50);not null"`
 	Description     string        `gorm:"type:varchar(200)"`
 	PhoneNumber     string        `gorm:"type:varchar(20);not null"`
@@ -15,4 +14,5 @@ type Alert struct {
 	IsActive        bool          `gorm:"type:boolean;not null"`
 	CreatedAt       time.Time     `gorm:"type:timestamptz;not null;default:now()"`
 	ModifiedAt      time.Time     `gorm:"type:timestamptz;not null;default:now()"`
+	AlertTemplate   AlertTemplate `gorm:"foreignKey:AlertTemplateID;references:ID"`
 }
--- internal/user/models/user.go
+++ internal/user/models/user.go
@@ -4,10 +4,10 @@ import "time"
 
 type User struct {
 	ID              int64     `gorm:"primaryKey"`
-	IDUserPlan      int64     `gorm:"type:bigserial;not null"`
-	UserPlan        UserPlan  `gorm:"foreignKey:IDUserPlan;references:ID"`
-	Email           string    `gorm:"type:varchar(100);not null"`
+	UserPlanID      int64     `gorm:"type:bigserial;not null"`
+	Email           string    `gorm:"type:varchar(100);not null;unique_index"`
 	LastPaymentDate time.Time `gorm:"type:timestamptz;not null"`
 	CreatedAt       time.Time `gorm:"type:timestamptz;not null;default:now()"`
 	ModifiedAt      time.Time `gorm:"type:timestamptz;not null;default:now()"`
+	UserPlan        UserPlan  `gorm:"foreignKey:UserPlanID;references:ID"`
 }
