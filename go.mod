bfa735c6ed8fa230217d6545abecf62c957708c1
edorguez <edro998@gmail.com>
2025-04-08 14:01:58 -0400

    remove mongodb and add gorm

--- go.mod
+++ go.mod
@@ -2,22 +2,10 @@ module github.com/edorguez/payment-reminder
 
 go 1.24.1
 
+require gorm.io/gorm v1.25.12
+
 require (
-	github.com/golang/snappy v0.0.4 // indirect
-	github.com/jackc/pgpassfile v1.0.0 // indirect
-	github.com/jackc/pgservicefile v0.0.0-20240606120523-5a60cdf6a761 // indirect
-	github.com/jackc/pgx/v5 v5.7.4 // indirect
-	github.com/jackc/puddle/v2 v2.2.2 // indirect
 	github.com/jinzhu/inflection v1.0.0 // indirect
 	github.com/jinzhu/now v1.1.5 // indirect
-	github.com/klauspost/compress v1.16.7 // indirect
-	github.com/xdg-go/pbkdf2 v1.0.0 // indirect
-	github.com/xdg-go/scram v1.1.2 // indirect
-	github.com/xdg-go/stringprep v1.0.4 // indirect
-	github.com/youmark/pkcs8 v0.0.0-20240726163527-a2c0da244d78 // indirect
-	golang.org/x/crypto v0.36.0 // indirect
-	golang.org/x/sync v0.12.0 // indirect
 	golang.org/x/text v0.23.0 // indirect
-	gorm.io/driver/postgres v1.5.11 // indirect
-	gorm.io/gorm v1.25.12 // indirect
 )
---
+++ internal/account/repository/user.go
@@ -0,0 +1,86 @@
+package repository
+
+import (
+	"context"
+	"fmt"
+	"time"
+
+	models "github.com/edorguez/payment-reminder/internal/account/models"
+	"gorm.io/gorm"
+)
+
+type UserRepository interface {
+	Create(ctx context.Context, user *models.User) error
+	FindByID(ctx context.Context, id uint) (*models.User, error)
+	FindByEmail(ctx context.Context, email string) *models.User
+	Update(ctx context.Context, id uint, newUser *models.User) error
+	Delete(ctx context.Context, id uint) error
+}
+
+type userRepository struct {
+	DB *gorm.DB
+}
+
+func NewUserRepository(DB *gorm.DB) UserRepository {
+	return &userRepository{
+		DB: DB,
+	}
+}
+
+func (r *userRepository) Create(ctx context.Context, user *models.User) error {
+	createdUser := r.DB.Create(user)
+	return createdUser.Error
+}
+
+func (r *userRepository) FindByID(ctx context.Context, id uint) (*models.User, error) {
+	var user models.User
+
+	r.DB.First(&user, id)
+	if user.ID == 0 {
+		return nil, fmt.Errorf("User not found")
+	}
+
+	return &user, nil
+}
+
+func (r *userRepository) FindByEmail(ctx context.Context, email string) *models.User {
+	var users []models.User
+
+	r.DB.Where("email LIKE ?", "%"+email+"%").Find(&users)
+	if len(users) == 0 {
+		return nil
+	}
+
+	return &users[0]
+}
+
+func (r *userRepository) Update(ctx context.Context, id uint, newUser *models.User) error {
+	var oldUser models.User
+
+	r.DB.First(&oldUser, id)
+
+	if oldUser.ID == 0 {
+		return fmt.Errorf("User not found")
+	}
+
+	oldUser.UserPlanID = newUser.UserPlanID
+	oldUser.Email = newUser.Email
+	oldUser.LastPaymentDate = newUser.LastPaymentDate
+	oldUser.ModifiedAt = time.Now()
+
+	return nil
+}
+
+func (r *userRepository) Delete(ctx context.Context, id uint) error {
+	var user models.User
+
+	r.DB.First(&user, id)
+
+	if user.ID == 0 {
+		return fmt.Errorf("User not found")
+	}
+
+	r.DB.Unscoped().Delete(&user)
+
+	return nil
+}
